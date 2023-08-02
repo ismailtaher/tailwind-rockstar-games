f6ec1c220c4e926a5d0b3bb795eb1fa00805240c
ismailtaher <ismail.taher@ymail.com>
2023-08-02 21:11:01 +0500

    Third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./build/*.html", ".build/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleX(0)" },
+          "100%": { transform: "scaleX(1)" },
+        },
+      },
+      animation: {
+        "open-menu": "open-menu 0.2s ease-in-out forwards",
+      },
+    },
   },
   plugins: [],
-}
-
+};
